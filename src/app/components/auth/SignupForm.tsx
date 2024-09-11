7ddf6f3ccaafa242604f200313110abd5f499ad5
Satyajeet Behera <jagajajpur3@gmail.com>
2024-09-12 02:23:04 +0530

    commit

--- src/app/components/auth/SignupForm.tsx
+++ src/app/components/auth/SignupForm.tsx
@@ -1,229 +1,3 @@
-// "use client";
-// import React, { FormEvent } from "react";
-// import Image from "next/image";
-// import styled from "styled-components";
-// import Link from "next/link";
-// import { FcGoogle } from "react-icons/fc";
-// import { FaGithub, FaLinkedin } from "react-icons/fa";
-// import signup_bottomCircle from "../../../assets/signup_bottomCircle.svg";
-// import signup_upperCircle from "../../../assets/signup_upperCircle.svg";
-// import { OAuthStrategy } from "@clerk/types";
-// import { GithubOauthProvider } from "@clerk/types";
-// import { LinkedinOauthProvider } from "@clerk/types";
-// import { useSignIn } from "@clerk/nextjs";
-
-// const GlassContainer = styled.div`
-//   position: relative;
-//   background: rgba(255, 255, 255, 0.1);
-//   border: 1px solid rgba(255, 255, 255, 0.2);
-//   backdrop-filter: blur(15px);
-//   border-radius: 20px;
-//   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-//   padding: 2rem;
-//   z-index: 1;
-// `;
-
-// const Circle = styled(Image)`
-//   position: absolute;
-//   z-index: 0;
-//   &:nth-child(1) {
-//     top: -50px;
-//     right: -100px;
-//     width: 200px;
-//     height: 200px;
-//   }
-//   &:nth-child(2) {
-//     bottom: -100px;
-//     left: -50px;
-//     width: 250px;
-//     height: 250px;
-//   }
-// `;
-
-// interface SignUpFormProps {
-//   signUpWithEmail: ({
-//     emailAddress,
-//     password,
-//   }: {
-//     emailAddress: string;
-//     password: string;
-//   }) => void;
-//   clerkError: string;
-// }
-
-// const SignupForm = ({ signUpWithEmail, clerkError }: SignUpFormProps) => {
-//   const { signIn } = useSignIn();
-
-//   if (!signIn) return null;
-
-//   const signInWithGoogle = (strategy: OAuthStrategy) => {
-//     return signIn.authenticateWithRedirect({
-//       strategy,
-//       redirectUrl: "/sign-up/sso-callback",
-//       redirectUrlComplete: "/",
-//     });
-//   };
-//   const signInWithGithub = (strategy: GithubOauthProvider) => {
-//     return signIn.authenticateWithRedirect({
-//       GithubOauthProvider,
-//       redirectUrl: "/sign-up/sso-callback",
-//       redirectUrlComplete: "/",
-//     });
-//   };
-//   const signInWithLinkedin = (strategy: LinkedinOauthProvider) => {
-//     return signIn.authenticateWithRedirect({
-//       strategy,
-//       redirectUrl: "/sign-up/sso-callback",
-//       redirectUrlComplete: "/",
-//     });
-//   };
-
-//   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     const target = e.target as typeof e.target & {
-//       email: { value: string };
-//       password: { value: string };
-//       username: { value: string };
-//       phonenumber: { value: string };
-//     };
-
-//     const email = target.email.value;
-//     const password = target.password.value;
-//     const username = target.username.value;
-//     const phonenumber = target.phonenumber.value;
-
-//     // Call the signUpWithEmail function with all fields
-//     signUpWithEmail({ emailAddress: email, password });
-//   };
-
-//   return (
-//     <div className="flex justify-center items-center h-screen bg-black text-white px-12 overflow-hidden relative">
-//       <div className="flex flex-col justify-start items-start space-y-8 w-[70%] p-8">
-//         <div className="text-[5rem] font-bold tracking-wide">
-//           Roll the Carpet!
-//         </div>
-
-//         <div className="flex items-center w-full">
-//           <div className="border-4 border-white px-6 py-3 text-lg">
-//             Skip&nbsp;the&nbsp;Lag&nbsp;?
-//           </div>
-//           <div className="w-full border-t-2 border-dashed border-white ml-4"></div>
-//         </div>
-//       </div>
-
-//       <div className="relative w-[30%] h-auto">
-//         <Circle src={signup_upperCircle} alt="Upper Circle" />
-//         <Circle src={signup_bottomCircle} alt="Bottom Circle" />
-
-//         <GlassContainer className="glass-container">
-//           <form className="space-y-4 relative z-10" onSubmit={handleSubmit}>
-//             <div>
-//               <div className="text-3xl font-semibold text-start">Sign Up</div>
-//             </div>
-//             <div className="text-sm font-semibold text-start">
-//               Just some details to get you in.!
-//             </div>
-
-//             <input
-//               type="text"
-//               id="username"
-//               name="username"
-//               placeholder="Username"
-//               className="w-full p-2 rounded-2xl bg-transparent border-2 text-lg border-white focus:outline-none"
-//               required
-//             />
-
-//             <input
-//               type="email"
-//               id="email"
-//               name="email"
-//               placeholder="Email Address"
-//               className="w-full p-2 rounded-2xl bg-transparent border-2 text-lg border-white placeholder-white focus:outline-none"
-//               required
-//             />
-
-//             <input
-//               type="tel"
-//               id="phonenumber"
-//               name="phonenumber"
-//               placeholder="Phone Number"
-//               className="w-full p-2 rounded-2xl bg-transparent border-2 text-lg border-white placeholder-white focus:outline-none"
-//               required
-//             />
-
-//             <input
-//               type="password"
-//               id="password"
-//               name="password"
-//               placeholder="Password"
-//               className="w-full p-2 rounded-2xl bg-transparent border-2 border-white placeholder-white text-lg focus:outline-none"
-//               required
-//             />
-
-//             {/* Display error message */}
-//             {clerkError && <p className="text-red-500">{clerkError}</p>}
-
-//             <div className="text-center">
-//               <button
-//                 type="submit"
-//                 className="w-full text-white py-3 rounded-2xl transition-colors duration-300"
-//                 style={{
-//                   background:
-//                     "linear-gradient(90deg, #2E4CEE 0%, #221EBF 53%, #040F75 100%)",
-//                 }}
-//               >
-//                 Sign Up
-//               </button>
-//             </div>
-
-//             <div className="flex items-center my-4">
-//               <div className="border-t-2 border-solid border-white w-1/2"></div>
-//               <p className="mx-4 text-sm">or</p>
-//               <div className="border-t-2 border-solid border-white w-1/2"></div>
-//             </div>
-
-//             <div className="flex space-x-5 justify-center">
-//               <button
-//                 type="button"
-//                 onClick={() => signInWithGoogle("oauth_google")}
-//               >
-//                 <FcGoogle className="h-10 w-10" />
-//               </button>
-//               <button
-//                 type="button"
-//                 onClick={() => signInWithGithub("oauth_github")}
-//               >
-//                 <FaGithub className="h-10 w-10" />
-//               </button>
-//               <button
-//                 type="button"
-//                 onClick={() => signInWithLinkedin("oauth_linkedin")}
-//               >
-//                 <FaLinkedin className="h-10 w-10" />
-//               </button>
-//             </div>
-
-//             <div className="flex justify-center">
-//               <Link href="/sign-in" className="no-underline">
-//                 <p className="text-sm">Already Registered? Login</p>
-//               </Link>
-//             </div>
-
-//             <div className="flex space-x-5 items-center justify-center">
-//               <p className="text-sm">Terms & Conditions</p>
-//               <p className="text-sm">Support</p>
-//               <p className="text-sm">Customer Care</p>
-//             </div>
-//           </form>
-//         </GlassContainer>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SignupForm;
-
-
 "use client";
 import React, { FormEvent, useState } from "react";
 import Image from "next/image";
@@ -267,9 +41,13 @@ interface SignUpFormProps {
   signUpWithEmail: ({
     emailAddress,
     password,
+    userName,
+    phoneNumber
   }: {
     emailAddress: string;
     password: string;
+    userName: string;
+    phoneNumber: string;
   }) => void;
   clerkError: string;
 }
@@ -317,9 +95,10 @@ const SignupForm = ({ signUpWithEmail, clerkError }: SignUpFormProps) => {
 
     const email = target.email.value;
     const password = target.password.value;
+    const userName = target.username.value;
+    const phoneNumber = target.phonenumber.value;
 
-    // Call the signUpWithEmail function with the form data
-    await signUpWithEmail({ emailAddress: email, password });
+    await signUpWithEmail({ emailAddress: email, password, userName, phoneNumber});
     setLoading(false);
   };
 
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,26 +1,36 @@
-// app/sign-up/[[...sign-up]]/page.tsx
 "use client";
 import Link from "next/link";
 import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
-import { useSignUp } from "@clerk/nextjs";
 import SignupForm from "@/app/components/auth/SignupForm";
 import VerifyForm from "@/app/components/auth/VerifyForm";
+import { useSignUp } from "@clerk/nextjs";
 
 const Signup = () => {
-  const {isLoaded, signUp, setActive} = useSignUp();
+  const { isLoaded, signUp, setActive } = useSignUp();
   const [clerkError, setClerkError] = useState("");
   const router = useRouter();
 
   const [verifying, setVerifying] = useState(false);
   const [code, setCode] = useState("");
+  const [userData, setUserData] = useState<{
+    emailAddress: string;
+    password: string;
+    userName: string;
+    phoneNumber: string;
+  } | null>(null);
 
+  // Step 1: Sign up the user with Clerk (prepare email verification)
   const signUpWithEmail = async ({
     emailAddress,
-    password
+    password,
+    userName,
+    phoneNumber,
   }: {
     emailAddress: string;
     password: string;
+    userName: string;
+    phoneNumber: string;
   }) => {
     if (!isLoaded) {
       return;
@@ -31,7 +41,13 @@ const Signup = () => {
         emailAddress,
         password,
       });
-      await signUp.prepareEmailAddressVerification({strategy: "email_code"});
+
+      // Save user data for later API request
+      setUserData({ emailAddress, password, userName, phoneNumber });
+
+      await signUp.prepareEmailAddressVerification({
+        strategy: "email_code",
+      });
 
       setVerifying(true);
     } catch (err: any) {
@@ -39,39 +55,61 @@ const Signup = () => {
     }
   };
 
+  // Step 2: Handle the verification and send data to the external API after success
   const handleVerify = async (e: FormEvent) => {
     e.preventDefault();
     if (!isLoaded) return;
-    
+
     try {
-      console.log("Degubber in try")
       const completeSignUp = await signUp.attemptEmailAddressVerification({
         code,
-        // strategy: 'email_code',
       });
-      if (completeSignUp.status !== "complete") {
-        console.log(JSON.stringify(completeSignUp, null, 2));
-      }
 
       if (completeSignUp.status === "complete") {
-        await setActive({session: completeSignUp.createdSessionId});
-        router.push("http://localhost:3000/");
+        await setActive({ session: completeSignUp.createdSessionId });
+
+        // Now send user data to the external API since email is verified
+        if (userData) {
+          const response = await fetch("http://127.0.0.1:8000/api/user/singup/", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username: userData.userName,
+              email: userData.emailAddress,
+              phone_number: userData.phoneNumber,
+              password: userData.password,
+            }),
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            console.log(data.message); // Expected: "Register successful"
+            router.push("/success");
+          } else {
+            const errorData = await response.json();
+            setClerkError(errorData.message || "Something went wrong");
+          }
+        }
+      } else {
+        console.log(JSON.stringify(completeSignUp, null, 2));
       }
     } catch (err) {
-      console.log("Degubber in catch")
+      setClerkError("Verification failed. Please try again.");
       console.log("Error:", JSON.stringify(err, null, 2));
     }
   };
 
   return (
     <>
-      {!verifying ?
-        (<SignupForm signUpWithEmail={signUpWithEmail} clerkError={clerkError} />) :
-        (<VerifyForm handleVerify={handleVerify} code={code} setCode={setCode} />)
-      }
+      {!verifying ? (
+        <SignupForm signUpWithEmail={signUpWithEmail} clerkError={clerkError} />
+      ) : (
+        <VerifyForm handleVerify={handleVerify} code={code} setCode={setCode} />
+      )}
     </>
-  )
-
+  );
 };
 
 export default Signup;
